65bfd59fc54dd32d00aa9e8a85715649290a45ef
DebasishBarai <debasishbaraiju@gmail.com>
2023-05-28 00:37:16 +0530

    next auth partially implemented with dummy user

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
+import { getServerSession } from 'next-auth';
 import Link from 'next/link';
-const Home = () => {
+import { authOptions } from './api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
+const Home = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect('/questions');
+  }
+
   return (
     <main className='bg-[#1E293B] min-h-screen min-w-full flex flex-col justify-center items-center'>
       <h1 className='text-lg text-[#28A0F1] p-4'>Welcome to meetcode</h1>
-      <Link href='/signin'>
+      <Link href='api/auth/signin'>
         <button className='text-[#28A0F1] min-w-[200px] border-[0.125em] border-[#28A0F1] border-solid rounded-lg p-2 m-2 shadow-[0_0_10px_2px_#28A0F1,_0_0_10px_2px_#28A0F1_inset] [text-shadow:_0_0_0.125em] hover:text-[#31202B] hover:bg-[#28A0F1] hover:shadow-[0_0_100px_10px_#28A0F1] hover:[text-shadow:_none]'>
           SignIn
         </button>
       </Link>
-      <Link href='/signup'>
+      <Link href='api/auth/signin'>
         <button className='text-[#28A0F1] min-w-[200px] border-[0.125em] border-[#28A0F1] border-solid rounded-lg p-2 m-2 shadow-[0_0_10px_2px_#28A0F1,_0_0_10px_2px_#28A0F1_inset] [text-shadow:_0_0_0.125em] hover:text-[#31202B] hover:bg-[#28A0F1] hover:shadow-[0_0_100px_10px_#28A0F1] hover:[text-shadow:_none]'>
           SignUp
         </button>
--- components/LayoutComponent.tsx
+++ components/LayoutComponent.tsx
@@ -1,6 +1,11 @@
 'use client';
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
 import { useState } from 'react';
+
 const LayoutComponent = () => {
+  const { data: session } = useSession();
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = () => setIsOpen(!isOpen);
@@ -62,7 +67,7 @@ const LayoutComponent = () => {
             </div>
             <div className='relative left-[20%] top-0 flex h-16 w-[80%] flex-col items-start justify-center border-b border-b-[#28A0F1]/[0.5]'>
               <button className='absolute p-4 text-[#28A0F1] hover:font-semibold'>
-                Logout
+                <Link href='/api/auth/signout'>Logout</Link>
               </button>
             </div>
           </div>
@@ -81,7 +86,9 @@ const LayoutComponent = () => {
                 d='M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z'
               />
             </svg>
-            <h1 className='p-4 text-[#28A0F1] w-[80%] truncate'>User name</h1>
+            <h1 className='p-4 text-[#28A0F1] w-[80%] truncate'>
+              {session?.user?.name}
+            </h1>
           </div>
         </div>
       </div>
